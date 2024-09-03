7ae98368c94dfb8aec6ae37cad8a580656dd56bf
iqShuten <shuten_tam@tut.by>
2024-09-03 16:02:58 +0300

    add DevSerev, development and production mods

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import { buildDevServer } from "./buildDevServer";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
@@ -8,7 +8,7 @@ import webpack from "webpack";
 export function buildWebpackConfig(
   options: buildOptions
 ): webpack.Configuration {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
 
   return {
     mode,
@@ -23,5 +23,7 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
