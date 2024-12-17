bb1c31325e3815b0ea4ac9812d4b0c0a07489253
iqShuten <shuten_tam@tut.by>
2024-12-17 11:49:41 +0300

    <temp branches problems>

---
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -0,0 +1,18 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./ProfilePage.module.scss";
+
+interface ProfilePageProps {
+  className?: string;
+}
+
+export const ProfilePage = ({ props }: ProfilePageProps) => {
+  const {
+    className,
+  } = props
+
+  return (
+    <div className={classNames(cls.ProfilePage, {}, [className])}>
+
+    </div>
+  );
+};
